cde35f9e28e5b049e1f49342492c869d545579e9
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-07-19 13:19:36 -0700

    string_max_index

---
+++ secret_agents.rb
@@ -0,0 +1,18 @@
+# method that takes a string
+# find string length to find string index max (lenght -1)
+# loop through index max to take each letter of the string
+# in the loop advance each letter using .next
+# save each new letter assingment and add them together to create a new string
+# make accomidation for z to be a, and spaces to stay spaces, and all are lowercase
+def encrypt(string)
+	string_length = string.length
+	string_max_index = string_length - 1
+end
+
+#driver code for string length - gives 6 as Fixnum
+# puts encrypt("length").class
+# puts encrypt("length")
+
+#driver code for string_max_index - gives 5 as Fixnum
+puts encrypt("length")
+puts encrypt("length").class
