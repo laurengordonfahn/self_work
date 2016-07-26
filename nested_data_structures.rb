24adfeeb86c7491f96e0c6f9ebf2f20089e68d9a
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-07-26 09:41:57 -0700

    remember to put commas on the same line that they are ending or it won't run, annoying

--- nested_data_structures.rb
+++ nested_data_structures.rb
@@ -1,23 +1,23 @@
 bookcase = {
 	'shelf1' => {
-		books: {"War and Peace"=> "Tolstoy", "Love in the Time of Cholera" => "Author Unknown"}
-		, things:["Lamp", "Magic 8 Ball"]
-	}
-	,'shelf2' => {
-		books: {"Alphabet Soup" => "Llyod Micpherson"}
-		, things:["Crystal", " Peace pipe"]
-	}
-	,'shelf3' => {
-		books: {"Song Bird" => "Lance Amstrong", "Call of the Wild" => "Jack London"}
-		, things: ["yarn collection", "knitting needles"]
+		books: {"War and Peace"=> "Tolstoy", "Love in the Time of Cholera" => "Author Unknown"},
+		 things:["Lamp", "Magic 8 Ball"],
+	},
+	'shelf2' => {
+		books: {"Alphabet Soup" => "Llyod Micpherson"},
+		 things:["Crystal", " Peace pipe"]
+	},
+	'shelf3' => {
+		books: {"Song Bird" => "Lance Amstrong", "Call of the Wild" => "Jack London"},
+		 things: ["yarn collection", "knitting needles"]
 	}
 }
 
 #expecting shelf 2
-bookcase['shelf2']
+puts bookcase['shelf2']
 
 #expecting "Crystal"
-bookcase['shelf2'][:things][0]
+puts bookcase['shelf2'][:things][0]
 
 #expecting Shelf 1's list of books
-bookcase['shelf1'][:books].keys
+puts bookcase['shelf1'][:books].keys
