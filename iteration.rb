95b6bfebc0560098490b8145d9c152161502b1c1
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-07-26 09:37:10 -0700

    I must learn with .each to put the put statements outside the loop so that way it only prints once instead of iterating through the whole count on each line

--- iteration.rb
+++ iteration.rb
@@ -2,14 +2,16 @@ array = ["item1", "item2", "item3"]
 hash = {name:"Lauren", height: 5.1, happy: true}
 
 array.each do |itemx|
-	x = 1
+	x = 0
 	until x > array.length
-		puts "this is #{itemx}  it is the #{array.index()} location in this array."
+		location = array.index(itemx) + 1
+		
 		x +=1	
 	end
+	puts "this is #{itemx} it is the #{location} location in this array."
 end
 
 hash.each do |key, value|
-	puts "this line is the value of #{key} is #{value}"
+	puts "this line has the key value of #{key} which is maped to #{value}"
 end
 
