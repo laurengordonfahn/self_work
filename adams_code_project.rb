8862dbb1c14ea6b1f7af71a62c8791b98f8420c6
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-07-30 11:52:00 -0700

    working on second portion

--- adams_code_project.rb
+++ adams_code_project.rb
@@ -13,24 +13,45 @@
 # puts decode("String. Thing!").class
 
 
+# def decode(string)
+# 	string_array = string.split("")
+
+# 	int_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# 	nearly_new_string = ""
+# 	string_array.each do |element|
+# 		if element == "0"
+# 			nearly_new_string = nearly_new_string
+# 		elsif element.to_i != 0
+# 			nearly_new_string = nearly_new_string
+# 		else 
+# 			nearly_new_string = nearly_new_string + element
+# 		end
+# 	end 
+
+# 	return nearly_new_string
+# end
+
+# puts decode("123Strings are Gr8!")
+# puts decode("123Strings are Gr8!").class
+
 def decode(string)
 	string_array = string.split("")
 
-	int_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+	int_string_array = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
+	int_gage = true
 	nearly_new_string = ""
+
 	string_array.each do |element|
-		if element == "0"
-			nearly_new_string = nearly_new_string
-		elsif element.to_i != 0
+		if int_string_array.inculde?(element)
+			int_gage = true
 			nearly_new_string = nearly_new_string
 		else 
+			int_gage = false
 			nearly_new_string = nearly_new_string + element
 		end
 	end 
 
 	return nearly_new_string
 end
-
-puts decode("123Strings are Gr8!")
-puts decode("123Strings are Gr8!").class
