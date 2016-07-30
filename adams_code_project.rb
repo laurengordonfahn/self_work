3924fd0b60a9300a86ecbba925b5c9609e9be6ec
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-07-30 12:18:45 -0700

    is_digit method

--- adams_code_project.rb
+++ adams_code_project.rb
@@ -35,29 +35,41 @@
 # puts decode("123Strings are Gr8!")
 # puts decode("123Strings are Gr8!").class
 
-def decode(string)
-	string_array = string.split("")
+# def decode(string)
+# 	string_array = string.split("")
 
-	int_string_array = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+# 	int_string_array = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-	int_gage = false
-	nearly_new_string = ""
-	index_counter = -1
+# 	int_gage = false
+# 	nearly_new_string = ""
+# 	index_counter = -1
 
-	string_array.each do |element|
-		if int_string_array.include?(element)
+# 	string_array.each do |element|
+# 		if int_string_array.include?(element)
+# 			int_gage = true
+# 		else 
+# 			int_gage = false
+# 		end
+# 	index_counter = index_counter + 1
+
+# 		if int_gage == true
+# 			nearly_new_string = nearly_new_string + string_array[(element.to_i + index_counter + 1)]
+# 		end
+
+# 	end 
+# 	return nearly_new_string
+# end
+
+# puts decode("1aH2abi4abcd!")
+
+def is_digit?(string)
+int_string_array = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+int_gage = false
+	if int_string_array.include?(element)
 			int_gage = true
-		else 
-			int_gage = false
-		end
-	index_counter = index_counter + 1
-
-		if int_gage == true
-			nearly_new_string = nearly_new_string + string_array[(element.to_i + index_counter + 1)]
-		end
-
-	end 
-	return nearly_new_string
+	else 
+		int_gage = false
+	end
 end
 
-puts decode("1aH2abi!")
+
